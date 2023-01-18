2c305c7efbfad4a97aebb3da1533d4694a55789c
Tudor Pitic <tudor.pitic@she.com>
2023-01-18 11:41:34 +0200

    Update docker-compose.yml with RabbitMQ configuration. Update app properties

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,5 @@ COPY --from=builder /root/snapshot-dependencies/ ./
 RUN sleep 10
 COPY --from=builder /root/spring-boot-loader/ ./
 COPY --from=builder /root/application/ ./
+EXPOSE 8080
 ENTRYPOINT ["java", "org.springframework.boot.loader.JarLauncher","-XX:+UseContainerSupport -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Xms512m -Xmx512m -XX:+UseG1GC -XX:+UseSerialGC -Xss512k -XX:MaxRAM=72m"]
--- src/main/java/com/ds/management/config/RabbitMQConfig.java
+++ src/main/java/com/ds/management/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,15 @@ public class RabbitMQConfig {
     public static final String EXCHANGE_NAME = "appExchange";
     public static final String QUEUE_NAME = "appQueue";
     public static final String ROUTING_KEY = "messages.key";
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
     @Bean
     public Queue appQueue() {
         return new Queue(QUEUE_NAME);
